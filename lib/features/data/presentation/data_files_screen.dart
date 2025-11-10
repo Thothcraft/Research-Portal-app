e070b883f843e8087d314ba530905fb24f1846a1
Gad <egad@uwo.ca>
2025-11-10 11:07:00 -0500

    Add data files screen

---
+++ lib/features/data/presentation/data_files_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DataFilesScreen extends StatelessWidget {
+  const DataFilesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Data Files')),
+      body: const Center(child: Text('Data Files Screen - Under Development')),
+    );
+  }
+}
